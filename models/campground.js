3a7efb896d75aa1050dcd9a9f5d856049bbcafe1
Kiyoung Heo <rldud035@naver.com>
2023-08-29 00:37:36 +0900

    mongoose virtual property

--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,21 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema; //일종의 단축어
 
+// 각 이미지를 cloudinary의 transform 가상 api 사용을 위해 이미지 스키마를 따로 생성. (왜냐하면 가상 특성은 스키마에만 추가할 수 있음)
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumnail').get(function () { // 가상 특성 thumnail을 ImageSchma에 추가. thumnail 속성이 사용되면 이미지 크기를 수정하는 로직이 실행
+    return this.url.replace('/upload', '/upload/w_200');
+    // 이렇게 가상 속성을 설정했으니 edit.ejs에서 img src = img.url이 아닌 img.thumnail을 사용
+});
+
 const CampgroundSchema = new Schema({ //mongoose.Schema 대신 Schema로 쓸 수 있게 됨
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
